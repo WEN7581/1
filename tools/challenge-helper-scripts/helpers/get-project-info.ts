43ec71ab1e820a64bdc6509a777a9c199a5e7ea5
WEN7581 <780923665@qq.com>
2026-03-22 09:39:02 +0800

    Delete tools directory

--- tools/challenge-helper-scripts/helpers/get-project-info.ts
+++
@@ -1,8 +0,0 @@
-export function getProjectPath(): string {
-  // eslint-disable-next-line turbo/no-undeclared-env-vars
-  return (process.env.INIT_CWD || process.cwd()) + '/';
-}
-
-export function getProjectName(): string {
-  return getProjectPath().split('/').slice(-2)[0];
-}
